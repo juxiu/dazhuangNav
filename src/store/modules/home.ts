753562618ad4428eccf97f2158c6373f10c490d4
郭林涛 <v-guolintao@sinochem.com>
2024-01-26 17:37:29 +0800

    feat: home

--- src/store/modules/home.ts
+++ src/store/modules/home.ts
@@ -5,10 +5,7 @@ import {ref} from 'vue'
 export const useHomeStore = defineStore('homeStore', () => {
     
 
-    const tags = ref([{name:'test',suffix:'😂'}])
-    for (let index = 0; index < 20; index++) {
-        tags.value.push({name:'test'+index,suffix:'😂'})
-    }
+    const tags = ref([{name:'test',suffix:'😂'},{name:'test',suffix:'😂'}])
 
     const setTags = (newTags) => { 
         tags.value = newTags
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,6 +6,103 @@ import { ref } from "vue"
 import { useHomeStore } from "@/store/modules/home.ts"
 const { tags, setTags } = useHomeStore()
 
+
+// const getTitle = (url) => {
+//     return fetch(`https://api.embed.ly/1/oembed?url=${url}`)
+//         .then((response) => response.text())
+//         .then((html) => {
+//             const doc = new DOMParser().parseFromString(html, "text/html");
+//             const title = doc.querySelectorAll('title')[0];
+//             return title.innerText;
+//         });
+// };
+
+// // 你可以传入一个网页的URL数组，然后获取它们的标题
+// const urls = [
+//     'https://www.pnpm.cn',
+//     'https://pinia.web3doc.top'
+// ];
+
+// Promise.all(urls.map((url) => getTitle(url)))
+//     .then((titles) => {
+//         console.log(titles);
+//     });
+
+// import ogs from 'open-graph-scraper';
+// const options = { url: 'https://www.pnpm.cn' };
+// ogs(options)
+//     .then((data) => {
+//         const { error, html, result, response } = data;
+//         console.log('error:', error);  // This returns true or false. True if there was an error. The error itself is inside the result object.
+//         console.log('html:', html); // This contains the HTML of page
+//         console.log('result:', result); // This contains all of the Open Graph results
+//         console.log('response:', response); // This contains response from the Fetch API
+//     })
+
+// 从其他网页获取Open Graph信息的函数
+function getOpenGraphData(url) {
+    // fetch('https://api.allorigins.win/get?url=' + encodeURIComponent(url))
+    fetch('http://anyorigin.com/go?url=' + encodeURIComponent(url))
+
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(data => {
+            const html = data.contents;
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(html, 'text/html');
+            const ogData = {
+                title: doc.querySelector('meta[property="og:title"]').getAttribute('content'),
+                description: doc.querySelector('meta[property="og:description"]').getAttribute('content'),
+                image: doc.querySelector('meta[property="og:image"]').getAttribute('content'),
+                url: doc.querySelector('meta[property="og:url"]').getAttribute('content')
+            };
+            console.log('Open Graph data:', ogData);
+            // 在这里使用Open Graph数据，例如显示预览信息
+        })
+        .catch(error => {
+            console.error('Error fetching Open Graph data:', error);
+        });
+}
+// 调用函数并传入目标网页的URL
+const targetUrl = 'https://www.pnpm.cn';
+getOpenGraphData(targetUrl);
+
+const data = [
+    {
+        "title": "xxx",
+        "des": "xxx",
+        "img": "xxx",
+        "url": "xxx",
+        "tags": ["xxx", "xxx"]
+    },
+    {
+        "title": "xxx",
+        "des": "xxx",
+        "img": "xxx",
+        "url": "xxx",
+        "tags": ["xxx", "xxx"]
+    },
+    {
+        "title": "xxx",
+        "des": "xxx",
+        "img": "xxx",
+        "url": "xxx",
+        "tags": ["xxx", "xxx"]
+    },
+    {
+        "title": "xxx",
+        "des": "xxx",
+        "img": "xxx",
+        "url": "xxx",
+        "tags": ["xxx", "xxx"]
+    },
+]
+
+
 const activeIndex = ref(0)
 </script>
 
@@ -30,12 +127,31 @@ const activeIndex = ref(0)
         <n-divider />
         <section class="">
             <n-grid :x-gap="12" :y-gap="8" :cols="6">
-                <n-grid-item v-for="item in 16">
-                    <n-card title="卡片">
-                        卡片内容
+                <n-grid-item v-for="item in data">
+                    <n-card :title="item.title" class="card-custom box-shadow-custom">
+                        {{ item.des }}
                     </n-card>
                 </n-grid-item>
             </n-grid>
         </section>
     </div>
 </template>
+
+<style>
+.card-custom {
+    cursor: pointer;
+    -webkit-transition: all 250ms cubic-bezier(0.02, 0.01, 0.47, 1);
+    transition: all 250ms cubic-bezier(.02, .01, .47, 1);
+}
+
+.card-custom:hover {
+    box-shadow: 0 16px 32px 0 rgba(48, 55, 66, 0.15);
+    transform: translate(0, -5px);
+    transition-delay: 0s !important;
+}
+
+.box-shadow-custom {
+    -webkit-box-shadow: 0 0.25rem 1rem rgba(48, 55, 66, 0.15);
+    box-shadow: 0 4px 16px rgba(48, 55, 66, 0.15);
+}
+</style>
