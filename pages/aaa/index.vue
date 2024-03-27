2bec3f043df5786c9244e54056ac6ef47108a6aa
郭林涛 <v-guolintao@sinochem.com>
2024-03-27 16:57:35 +0800

    feat: ssr

--- pages/aaa/index.vue
+++ pages/aaa/index.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 <template>
     <h1>index</h1>
+    <n-button>Default</n-button>
+    <n-button type="tertiary">
+      Tertiary
+    </n-button>
     <!-- <n-button>Default</n-button> -->
 </template>
